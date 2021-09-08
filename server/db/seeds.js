a5939a98b65e4465a9d44397227877ffed1949e8
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-09-08 11:11:11 +0100

    Seeds created

---
+++ server/db/seeds.js
@@ -0,0 +1,25 @@
+use hotel;
+db.dropDatabase();
+
+db.bookings.insertMany([ 
+    {
+        name: "Ringo Starr",
+        email: "ringostarr@gmail.com",
+        status: true
+    },
+    {
+        name: "John Lennon",
+        email: "johnlennon@gmail.com",
+        status: false
+    },
+    {
+        name: "Paul McCartney",
+        email: "paulmccartney@gmail.com",
+        status: true
+    },
+    {
+        name: "George Harrison",
+        email: "georgeharrison@gmail.com",
+        status: false
+    }
+])
---
+++ server/helpers/create_router.js
@@ -0,0 +1,2 @@
+const express = require('express');
+const ObjectID = require('mongo.db').ObjectID;
