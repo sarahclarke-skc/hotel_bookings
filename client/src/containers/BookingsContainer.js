97e7744315c9fd16eef417da547050d5521f83c7
mitkousa <uk.dimitar@gmail.com>
2021-09-08 13:30:41 +0100

    bookings container done

---
+++ client/src/containers/BookingsContainer.js
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from "react";
+import { getBookings } from "../BookingsService";
+
+
+const BookingsContainer = () => {
+
+    const [hotelBookings, setHotelBookings] = useState([]);
+    useEffect (() => {
+        getBookings()
+        .then((allBookings) => {
+            setHotelBookings(allBookings);
+        })
+    }, [])
+
+    const addBooking = (booking) => {
+        const temp = hotelBookings.map(b => b);
+        temp.push(booking);
+        setHotelBookings(temp)
+    }
+
+    const removeBooking = (id) => {
+        const temp = hotelBookings.map(b => b);
+        const indexToDelete = temp.map(b => b._id).indexOf(id);
+        temp.splice(indexToDelete, 1);
+        setHotelBookings(temp);
+    }
+
+    return (
+        <>
+        <BookingsForm addBooking={addBooking} />
+        <BookingsList bookings = {bookings} removeBooking={removeBooking}/>
+        </>
+    )
+}
