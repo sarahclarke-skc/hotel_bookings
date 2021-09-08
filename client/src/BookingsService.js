50ae20aaad4574dca56bd4f1ec152fbc80370daa
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-09-08 12:01:32 +0100

    Client git folder fixed

---
+++ client/src/BookingsService.js
@@ -0,0 +1,24 @@
+const baseURL = 'http://localhost:5000/api/bookings/'
+
+export const getBookings = () => {
+    return fetch(baseURL)
+        .then(res => res.json())
+}
+
+export const postBooking = (payload) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+}
+
+export const deleteBooking = (id) => {
+    return fetch(baseURL + id, {
+        method: 'DELETE'
+    })
+}
+
+//TO DO add an update
+//checking git branching thing
