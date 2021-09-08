cdc8ffd520860da36b4ccfab30a6b8419aa02398
mitkousa <uk.dimitar@gmail.com>
2021-09-08 14:01:18 +0100

    booking item done

--- client/src/components/BookingItem.js
+++ client/src/components/BookingItem.js
@@ -1,2 +1,21 @@
 import React from "react";
 import { deleteBooking } from "../BookingsService";
+
+const BookingItem = ({booking, removeBooking}) => {
+    const handleDelete = () => {
+        deleteBooking(booking._id)
+        .then(()=> {
+            removeBooking(booking._id);
+        })
+    }
+    return (
+        <>
+        <h2>Guest Name: {booking.name}</h2>
+        <p>Email: {booking.email}</p>
+        <p>Checked In:{booking.status}</p>
+        <button onClick = {handleDelete} />
+        </>
+    )
+}
+
+export default BookingItem
